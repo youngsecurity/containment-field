01c08588ca1cc20223e7a7a301c1b7e8a242864c
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-01-10 16:18:45 -0500

    +ubuntu-kde.sh

---
+++ docker/containers/ubuntu-kde/ubuntu-kde.sh
@@ -0,0 +1,1 @@
+docker run -itd --name=ubuntu-kde -h ubuntu-kde --privileged -e PUID=1000 -e PGID=1000 -e TZ=America/New_York -p 1000:3000 --net=Transit --dns=10.0.255.46  --restart=always linuxserver/webtop:ubuntu-kde
