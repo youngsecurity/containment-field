acf323372793b6c755d0340457f06219057242e7
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-03-08 23:46:31 -0500

    +modified ubuntu-22.sh to include sysctl.conf rootless configuration requirements

--- hosts/ubuntu-22.sh
+++ hosts/ubuntu-22.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # sudoers.d setup
 sudo touch /etc/sudoers.d/devusr
 sudo visudo --file=/etc/sudoers.d/devusr
@@ -5,11 +7,11 @@ devusr ALL=(ALL) NOPASSWD:ALL
 # or
 sudo echo "devusr ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/devusr
 
-# apt update and upgrage
+# apt update and upgrade
 sudo apt update
 sudo apt -y upgrade
 
-# Ubuntu UFW
+# Ubuntu UFW Configuration
 sudo ufw allow from 10.0.0.0/8 to any port 9001
 sudo ufw allow from 10.0.0.0/8 to any port 1234
 
@@ -25,3 +27,8 @@ net.ipv4.ping_group_range = 0 2147483647
 # The default is set to 1024, which means only processes running as
 # root can bind to ports below 1024
 net.ipv4.ip_unprivileged_port_start = 0
+
+###################################################################
+# After modifying /etc/sysctl.conf, run the following command to 
+# activate the changes.
+sudo sysctl -p
