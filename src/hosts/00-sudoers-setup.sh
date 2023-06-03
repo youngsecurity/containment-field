25de2ebccabfb5dd883de73ebbc11aa50324efa8
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-06-02 20:45:52 -0400

    Update 00-sudoers-setup.sh

--- src/hosts/00-sudoers-setup.sh
+++ src/hosts/00-sudoers-setup.sh
@@ -2,15 +2,10 @@
 ###################################################################
 # sudoers.d setup
 # 
-# Save this script as create_sudoers_file.sh and make it executable 
-# using chmod +x create_sudoers_file.sh. Then, run the script as the 
-# user for whom you want to create the sudoers file using sudo 
-# ./create_sudoers_file.sh. The script will create a file in the 
-# /etc/sudoers.d/ directory with the name of the user and the 
-# necessary permissions.
-#
 # This will allow the logged in user to perform sudo without being 
 # required to enter their password.
+#
+###################################################################
 
 # Get the name of the current user
 USERNAME=$(whoami)
@@ -23,10 +18,13 @@ FILE="/etc/sudoers.d/$USERNAME"
 sudo touch $FILE
 
 # Add the necessary permissions to the file
-sudo echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" > $FILE
+sudo echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee $FILE > /dev/null
 
 # Set the appropriate file permissions
 sudo chmod 0440 $FILE
+
+# Verify the sudoers file for any syntax errors
+sudo visudo -cf $FILE
 
 # Print a success message
 echo "Sudoers file created for user $USERNAME at $FILE"
