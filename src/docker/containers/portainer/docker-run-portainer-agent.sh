28a3801c1c8e29b90cf7261b97d1669ecc92755b
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-06-13 04:49:29 -0400

    Update Portainer agent:2.18.3

--- src/docker/containers/portainer/docker-run-portainer-agent.sh
+++ src/docker/containers/portainer/docker-run-portainer-agent.sh
@@ -6,4 +6,4 @@ docker run -itd \
   --restart=always \
   -v /$XDG_RUNTIME_DIR/docker.sock:/var/run/docker.sock \
   -v ~/.local/share/docker/volumes:/var/lib/docker/volumes \
-  portainer/agent:2.16.2
+  portainer/agent:2.18.3
