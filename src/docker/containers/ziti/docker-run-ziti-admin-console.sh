3b336b1cda43fa93ff8c859ee37a8bac89fb6126
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-08-26 04:03:08 -0400

    update zac deploy

--- src/docker/containers/ziti/docker-run-ziti-admin-console.sh
+++ src/docker/containers/ziti/docker-run-ziti-admin-console.sh
@@ -23,8 +23,8 @@ deploy_ziti_admin_console() {
         --network macvlan255 \
         -p 1408:1408 \
         -p 8443:8443 \
-        -v "${HOME}/docker-volume/macvlan255/ziti-edge-controller-intermediate/keys/ziti-edge-controller-server.key":/usr/src/app/server.key \
-        -v "${HOME}/docker-volume/macvlan255/ziti-edge-controller-intermediate/certs/ziti-edge-controller-server.chain.pem":/usr/src/app/server.chain.pem \
+        -v "myPersistentZitiFiles:/persistent/macvlan255/ziti-edge-controller-intermediate/keys/ziti-edge-controller-server.key":/usr/src/app/server.key \
+        -v "myPersistentZitiFiles:/persistent/macvlan255/ziti-edge-controller-intermediate/certs/ziti-edge-controller-server.chain.pem":/usr/src/app/server.chain.pem \
         openziti/zac
 }
 
