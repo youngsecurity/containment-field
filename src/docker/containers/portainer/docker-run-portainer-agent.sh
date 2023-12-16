70844b13337020c1c8de120ad548fdd83455f82e
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-12-15 23:08:40 -0500

    update docker run portainer scripts

--- src/docker/containers/portainer/docker-run-portainer-agent.sh
+++ src/docker/containers/portainer/docker-run-portainer-agent.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # Setup Portainer Agent using rootless Docker user namespace
 docker run -itd \
   -p 9001:9001 \
   --name portainer_agent \
   --restart=always \
-  -v /$XDG_RUNTIME_DIR/docker.sock:/var/run/docker.sock \
+  -v /"$XDG_RUNTIME_DIR"/docker.sock:/var/run/docker.sock \
   -v ~/.local/share/docker/volumes:/var/lib/docker/volumes \
-  portainer/agent:2.18.3
+  portainer/agent:2..3
--- src/docker/containers/portainer/docker-run-portainer.sh
+++ src/docker/containers/portainer/docker-run-portainer.sh
@@ -1,13 +1,68 @@
-#!/bin/sh
+#!/bin/bash
+
+# .SCRIPT NAME: docker-run-update-portainer.sh
+# .AUTHOR: Joseph Young <joe@youngsecurity.net>
+# .DATE: 12/04/2023
+# .DOCUMENTATION:
+#   This script will perform the following tasks:
+#       1. Pull the latest image from Docker Hub
+#       2. Remove the Docker container
+#       3. Setup the Docker container and run it detached, interactive and allocate a pseudo-TTY
+# .DESCRIPTION: This is a Docker run shell script to update a Docker container.
+# .EXAMPLE: ./docker-run-update-portainer.sh <arguments>
+
+# Exit immediately if a command exits with a non-zero status
+#set -e
+
+# Main code
+# Notify the user the script has started.
+echo "Starting the script!"
+
+# Check GitHub for latest release version
+curl -s https://api.github.com/repos/portainer/portainer/releases/latest | jq -r '.tag_name'
+
+# Check Docker Hub for latest Docker image version
+imageSource=portainer/portainer-ce:latest
+docker pull $imageSource
+docker image inspect $imageSource | jq -r '.[].RepoDigests[]' | awk -F@ '{print $2}'
+# shellcheck disable=SC1035
+digest=$(!!)
+version=$(curl -s 'https://hub.docker.com/v2/repositories/portainer/portainer-ce/tags' -H 'Content-Type: application/json' | jq -r '.results[] | select(.digest == "'"$digest"'") | .name' | sed -n 2p)
+
+
+# Setup container using specific tag, because the :latest tag does not always pull down the latest version
+echo "Pulling Docker image..."
+docker pull portainer/portainer-ce:"$version"
+
+# Shutting down the stack
+#echo "Shutting down the stack..."
+#docker compose down
+
+# Stopping the container
+echo "Stopping the container..."
+docker container stop portainer
+
+# Delete existing container
+echo "Deleting existing container..."
+docker rm -f portainer
+echo "Container removed!"
 
 # First, create the volume that Portainer Server will use to store its database:
-docker volume create portainer_data
+#docker volume create portainer_data
 
-# Then, download and install the Portainer Server container:
+# Starting the stack...
+#echo "Starting up the stack..."
+#docker compose up -d
+
+# Starting up the container...
+echo "Starting up the container..."
 docker run -itd \
--p 8000:8000 -p 9443:9443 \
---name portainer \
---restart=unless-stopped \
--v /var/run/docker.sock:/var/run/docker.sock \
--v portainer_data:/data \
-portainer/portainer-ce:latest
+    -p 8000:8000 -p 9443:9443 \
+    --name portainer \
+    --restart=unless-stopped \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:"$version"
+
+# Notify the user the script has completed.
+echo "Script has finished!"
