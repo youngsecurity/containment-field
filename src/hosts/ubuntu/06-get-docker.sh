626f40e0b64ebfea4a02612ba8036da16e1378b6
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-01-31 04:06:39 -0500

    Update Ubuntu script to install latest Docker
    Enhanced the Docker installation script for Ubuntu to ensure removal of
    any outdated Docker packages before setup. Added steps to import
    Docker's official GPG key, added the Docker repository to the system's
    sources, and installed the latest Docker packages, including the buildx
    and compose plugins. The script now also adds the current user to the
    Docker group to eliminate the need for sudo when running Docker
    commands. Additionally, configured Docker to start on boot using
    systemd, providing a smoother user experience and aligning with best
    practices for Docker installation.

--- src/hosts/ubuntu/06-get-docker.sh
+++ src/hosts/ubuntu/06-get-docker.sh
@@ -4,6 +4,42 @@
 # 06-get-docker.sh 
 # 
 # This script will install docker on Ubuntu
+# https://docs.docker.com/engine/install/ubuntu/
 #
 ###################################################################
 
+# Uninstall old versions
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the latest Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Create the docker group.
+sudo groupadd docker
+
+# Add your user to the docker group.
+sudo usermod -aG docker "$USER"
+
+# Activate the changes to groups.
+newgrp docker
+
+# Verify that the DE installation is a success by running the `hello world` image and that you can run docker commands without sudo.
+docker run hello-world
+
+# Configure Docker to start on boot with systemd
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
