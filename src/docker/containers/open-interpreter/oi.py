2b1ccd21183806ccc95078d944d2e6bad830e784
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-02-22 09:17:16 -0500

    Update interpreter model source and enhance chat invocation
    The script has been updated to reference the interpreter language model from a more accurate source, ensuring consistency with external repositories. Additionally, the default chat invocation has been made more flexible by removing the hardcoded command and commenting it out for optional use, which can facilitate easier customization for user-specific tasks.

--- src/docker/containers/open-interpreter/oi.py
+++ src/docker/containers/open-interpreter/oi.py
@@ -1,7 +1,8 @@
 from interpreter import interpreter
 
 interpreter.offline = True # Disables online features like Open Procedures
-interpreter.llm.model = "dolphin-mistral:latest"
+interpreter.llm.model = "ollama/dolphin-mistral:latest"
 interpreter.llm.api_base = "http://10.0.255.147:11434/v1"
 
-interpreter.chat("install ping and check if 8.8.8.8 is up") # Executes a single command
+interpreter.chat() # Executes a single command
+#interpreter.chat("install ping and check if 8.8.8.8 is up")
