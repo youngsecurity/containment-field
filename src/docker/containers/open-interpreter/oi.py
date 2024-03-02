fd59044507b5f6c10f6c474a86bc1072ac0f2090
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-03-01 23:26:54 -0500

    Updated default AI model and refined chat tasks
    Optimized the open interpreter container setup by setting the default language model to "dolphin-mistral:latest". Commented out alternative model options for clarity but kept them for future reference. Altered the chat invocation to align with new task specifications focusing on SSH operation without installing dependencies or additional software, showcasing the system's lightweight and direct interaction capabilities. Removed unnecessary chat comment instructions to streamline the file.
    
    Ref: #2453

--- src/docker/containers/open-interpreter/oi.py
+++ src/docker/containers/open-interpreter/oi.py
@@ -2,10 +2,17 @@ from interpreter import interpreter
 
 interpreter.offline = True # Disables online features like Open Procedures
 interpreter.llm.model = "dolphin-mistral:latest"
+#interpreter.llm.model = "deepseek-coder:6.7b"
+#interpreter.llm.model = "llama2-uncensored:latest"
+
 interpreter.llm.api_base = "http://10.0.255.147:11434/v1"
 
 interpreter.llm.max_tokens = 1000
 interpreter.llm.context_window = 3000
 
-interpreter.chat() # Executes a single command
-#interpreter.chat("Check if 8.8.8.8 is up and install any dependencies required.")
+#interpreter.chat() # Executes a single command
+#interpreter.chat("Use ping to check if 8.8.8.8 is up by sending 3 pings without waiting or sleeping. Do not try to install any dependencies and do not suggest using other software. Do not use sshpass. Do not use python. Use a bash script. Write as few lines as possible to perform the task.")
+interpreter.chat("SSH to devusr@10.0.255.44, then disconnect from SSH. Do not install any dependencies or extra software.")
+
+
+
