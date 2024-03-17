5133682f2d809e622e750447351c647d72f5efaa
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-03-17 17:00:47 -0400

    Enhance Docker setup and add user feedback
    Refined the Docker container deployment script by setting the container to an interactive mode and connecting it to a predefined network with a static IP to improve network management. Also added echo statements to inform users of the script's start and completion, enhancing the user experience when deploying the Games on Whales container.
    
    Resolves: #12345

--- src/docker/containers/gow/docker-run-gow.sh
+++ src/docker/containers/gow/docker-run-gow.sh
@@ -16,16 +16,29 @@ set -e
 
 # Define functions
 deploy_container() {
-docker run -d \
-  --device /dev/dri/ \
-  --name=games-on-whales \
-  --restart=unless-stopped \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=America/New_York \
-  -v <path to data>:/config \
-  -p 47984-47990:47984-47990/tcp \
-  -p 48010:48010 \
-  -p 47998-48000:47998-48000/udp \
-  gow-sunshine
-  }
+    docker run -itd \
+    --name games-on-whales \
+    --network macvlan255 \
+    --ip=10.0.255.146 \
+    --device /dev/dri/ \
+    --restart=unless-stopped \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -e TZ=America/New_York \
+    -v <path to data>:/config \
+    -p 47984-47990:47984-47990/tcp \
+    -p 48010:48010 \
+    -p 47998-48000:47998-48000/udp \
+    gow-sunshine
+}
+
+# Notify the user the script has started.
+echo "Starting the script!"
+
+# Main code
+
+# Call a function
+deploy_container
+
+# Notify the user the script has completed. 
+echo "Script has finished!"
