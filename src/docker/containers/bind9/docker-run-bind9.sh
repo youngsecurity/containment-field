94df459395fc39f4a3f9d1b089c969c3fca081a1
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-04-29 05:40:00 -0400

    Added shebang to docker-run-gow shell script
    A shebang line has been introduced at the beginning of the 'docker-run-gow.sh' script to specify the script interpreter as bash. This ensures that the script is executed in a consistent environment, improving portability and reducing the likelihood of runtime errors on different systems.

---
+++ src/docker/containers/bind9/docker-run-bind9.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+# Define variables
+name="ns1"
+network="macvlan255"
+ip="10.0.255.24"
+timezone="TZ=America/New_York"
+image="ubuntu/bind9:9.18-22.04_beta"
+
+# Define functions
+deploy_container() {
+    docker run -itd \
+    --name $name \
+    --network $network \
+    --ip=$ip \
+    --restart=unless-stopped \
+    -e BIND9_USER=root \
+    -e $timezone \
+    -v ./config:/etc/bind \
+    -v ./cache:/var/cache/bind \
+    -v ./records:/var/lib/bind \
+    -p 53:53/tcp \
+    -p 53:53/udp \
+    $image
+}
+
+# Notify the user the script has started.
+echo "Starting the script!"
+
+# Main code
+
+# Call a function
+deploy_container
+
+# Notify the user the script has completed. 
+echo "Script has finished!"
