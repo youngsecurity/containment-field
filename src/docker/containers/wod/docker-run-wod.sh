a3b3a5a13b3f7c67e2ce470486ff6c2d4c4fd980
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-07-07 18:15:24 -0400

    +create docker-run-wod.sh

---
+++ src/docker/containers/wod/docker-run-wod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# .AUTHOR: Joseph Young <joe@youngsecurity.net>
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Main code
+echo -e "Script is running..."
+echo ""
+#LOG_FILE="./run.log" # Log file for errors and output messages.
+
