e0b1869ce81f200f85fcc690d75ea05920a0dc43
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-02-09 00:55:29 -0500

    Switch to Docker provider in Terraform setup
    Replaces AWS provider with Docker in Terraform configuration.
    Adds Docker resources for image, volume, network, and container.
    Enhances configuration with network, mounting, and restart
    policy settings for Docker containers.

--- src/boilerplate/OpenTofu/main.tf
+++ src/boilerplate/OpenTofu/main.tf
@@ -6,20 +6,72 @@
 # Once you have created your Terraform configuration file (e.g. main.tf), you can run terraform init to initialize the Terraform environment, terraform plan to see what changes Terraform will make, and terraform apply to apply those changes and provision the infrastructure.
 
 # Define provider
-provider "aws" {
-  region = "us-east-1"
+terraform {  
+  required_providers {
+
+#    aws = {
+#      region = "us-east-1"
+#    }
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+provider "docker" {
+  # Configuration options  
+  host = "tcp://ignignokt.home.youngsecurity.net:2376/"  
+  ca_material   = file(pathexpand("~/.docker/ca-docker.pem")) # this can be omitted
+  cert_material = file(pathexpand("~/.docker/client-docker-cert.pem"))
+  key_material  = file(pathexpand("~/.docker/key-docker-client.pem"))  
 }
 
-# Define variables
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+# Pull the Docker image
+resource "docker_image" "IMAGENAME" {
+  name = "IMAGENAME"
+}
 
-# Define resources
-#resource "aws_instance" "example" {
-#  ami           = "ami-0c55b159cbfafe1f0"
-#  instance_type = "t2.micro"
-#}
+# Reference the existing external volume
+resource "docker_volume" "VOLUMENAME" {
+  name = "VOLUMENAME"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
+# Reference the existing external network
+resource "docker_network" "NETWORKNAME" {
+  name = "NETWORKNAME"
+  driver = "macvlan"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "docker_network" "NETWORKNAME" {
+  name = "NETWORKNAME"  
+}
+
+# Create the Docker container
+resource "docker_container" "CONTAINERNAME" {  
+  image = docker_image.CONTAINERNAME.image_id
+  name  = "NAME"
+  hostname = "HOSTNAME"  
+  gpus = "device=GPU-fcc90235-d4c3-65e4-f064-446367f1cb5c"
+  tty = true
+
+  # Set environment variables
+  env = [
+    "TZ=America/New_York",
+  ]
+
+  # Add capabilities
+  #capabilities {
+  #  add = ["NET_ADMIN"]
+  #}
+  
   # Add devices
   #devices {
   #  host_path      = ""
@@ -50,6 +102,16 @@ variable "aws_secret_key" {}
   #  protocol = "udp"
   #}
 
+  # Set the stop timeout (in seconds)
+  #stop_timeout = 120
+
+  # Mount the external volume
+  mounts {
+    target = "/path/in/container"
+    source = resource.docker_volume.VOLUMENAME.name
+    type   = "volume"
+  }
+
   # Mount the external volumes
   # Mount /tmp/.X11-unix from the host to the container
   #mounts {
@@ -58,21 +120,31 @@ variable "aws_secret_key" {}
   #  type   = "bind"
   #}
 
+  # Connect to the external network with a static IP
+  networks_advanced {
+    name         = data.docker_network.NETWORKNAME.name
+    ipv4_address = "IP"
+  }
+
+  # Set the restart policy
+  restart = "unless-stopped"
+}
+# Define variables
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+
+#resource "aws_instance" "example" {
+#  ami           = "ami-0c55b159cbfafe1f0"
+#  instance_type = "t2.micro"
+#}
+
   # Encrypt the root block device
 #  ebs_block_device {
 #    device_name = "/dev/sda1"
 #    encrypted   = true
 #  }
 
-  # Set the stop timeout (in seconds)
-  #stop_timeout = 120
-
-  # Add capabilities
-  #capabilities {
-  #  add = ["NET_ADMIN"]
-  #}
-    
 # Define outputs
-output "public_ip" {
-  value = aws_instance.example.public_ip
-}
+#output "public_ip" {
+#  value = aws_instance.example.public_ip
+#}
---
+++ src/docker/containers/traefik/main.tf
@@ -0,0 +1,134 @@
+terraform {  
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+provider "docker" {
+  # Configuration options  
+  host = "tcp://ignignokt.home.youngsecurity.net:2376/"  
+  ca_material   = file(pathexpand("~/.docker/ca-docker.pem")) # this can be omitted
+  cert_material = file(pathexpand("~/.docker/client-docker-cert.pem"))
+  key_material  = file(pathexpand("~/.docker/key-docker-client.pem"))  
+}
+
+# Pull the Docker image
+resource "docker_image" "traefik:v3.3" {
+  name = "treafik:v3.3.3"
+}
+
+# Reference the existing external volume(s)
+resource "docker_volume" "open-webui" {
+  name = "open-webui"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "docker_volume" "open-webui" {
+  name = "open-webui"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Reference the existing external network
+resource "docker_network" "macvlan255" {
+  name = "macvlan255"
+  driver = "macvlan"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "docker_network" "macvlan255" {
+  name = "macvlan255"  
+}
+
+# Create the Docker container
+resource "docker_container" "open-webui" {  
+  image = docker_image.open-webui.image_id
+  name  = "open-webui"
+  hostname = "open-webui"  
+  gpus = "device=GPU-fcc90235-d4c3-65e4-f064-446367f1cb5c"
+  tty = true
+
+  # Set environment variables
+  env = [
+    "TZ=America/New_York",
+    "OLLAMA_BASE_URL=http://ollama:11434",
+  ]
+
+  # Mount the external volume
+  mounts {
+    target = "/app/backend/data"
+    source = resource.docker_volume.open-webui.name
+    type   = "volume"
+  }
+
+  # Connect to the external network with a static IP
+  networks_advanced {
+    name         = data.docker_network.macvlan255.name
+    ipv4_address = "10.0.255.148"
+  }
+
+  # Set the restart policy
+  restart = "unless-stopped"
+}# Reference the existing external volume
+resource "docker_volume" "open-webui" {
+  name = "open-webui"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Reference the existing external network
+resource "docker_network" "macvlan255" {
+  name = "macvlan255"
+  driver = "macvlan"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "docker_network" "macvlan255" {
+  name = "macvlan255"  
+}
+
+# Create the Docker container
+resource "docker_container" "open-webui" {  
+  image = docker_image.open-webui.image_id
+  name  = "open-webui"
+  hostname = "open-webui"  
+  gpus = "device=GPU-fcc90235-d4c3-65e4-f064-446367f1cb5c"
+  tty = true
+
+  # Set environment variables
+  env = [
+    "TZ=America/New_York",
+    "OLLAMA_BASE_URL=http://ollama:11434",
+  ]
+
+  # Mount the external volume
+  mounts {
+    target = "/app/backend/data"
+    source = resource.docker_volume.open-webui.name
+    type   = "volume"
+  }
+
+  # Connect to the external network with a static IP
+  networks_advanced {
+    name         = data.docker_network.macvlan255.name
+    ipv4_address = "10.0.255.148"
+  }
+
+  # Set the restart policy
+  restart = "unless-stopped"
+}
