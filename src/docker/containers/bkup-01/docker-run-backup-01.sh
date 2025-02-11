3b601cda803c51ebcec26ef83d999cc136a17137
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-02-11 05:47:12 -0500

    Updates docker run option for interactive mode
    Changes '--rm' to '--itd' in the docker run command to
    enable interactive and detached mode concurrently,
    potentially improving usability and control over the
    backup container operations.

--- src/docker/containers/bkup-01/docker-run-backup-01.sh
+++ src/docker/containers/bkup-01/docker-run-backup-01.sh
@@ -8,7 +8,7 @@ image="alpine"
 
 # Define functions
 deploy_container() {
-    docker run --rm \
+    docker run --itd \
         --name $name \
         -e $timezone \
         -v pihole_etc:/data \
