a59c5dd3e775d4314c2099429f3747d5d16e04d3
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-02-11 08:39:01 -0500

    +update docs -remove deprecated pihole update script

--- src/docker/containers/pihole/disable-systemd-resolved.sh
+++ src/docker/containers/pihole/disable-systemd-resolved.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 ##################################################
 ### Disable systemd-resolved DNS stub resolver ###
 ##################################################
-# When using a Docker Bridged network type, modern releases of Ubuntu (17.10+) and Fedora (33+) include systemd-resolved which uses a caching DNS stub resolver and prevents pi-hole from listening on port 53
+# When using a Docker Bridged network type, modern releases of Ubuntu (17.10+) and Fedora (33+) include systemd-resolved
+# which uses a caching DNS stub resolver and prevents pi-hole from listening on port 53
 # The stub resolver should be disabled with: https://github.com/pi-hole/docker-pi-hole#installing-on-ubuntu-or-fedora
 sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
 sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
