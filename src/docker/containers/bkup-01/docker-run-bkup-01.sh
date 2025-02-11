f3c18925ee7316bdfa04a59618b31c8cc05c1bf0
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-02-11 03:17:02 -0500

    Simplifies bkup-01 Docker container setup
    Removes unused network and IP configuration comments
    Eliminates unnecessary environment variable for bind user
    Switches volume mount syntax for clarity and consistency

--- src/docker/containers/bkup-01/docker-run-bkup-01.sh
+++ src/docker/containers/bkup-01/docker-run-bkup-01.sh
@@ -3,8 +3,6 @@ set -e
 
 # Define variables
 name="bkup-01"
-#network="macvlan255"
-#ip="10.0.255.24"
 timezone="TZ=America/New_York"
 image="alpine"
 
@@ -12,10 +10,8 @@ image="alpine"
 deploy_container() {
     docker run --rm \
         --name $name \
-        --restart=unless-stopped \
-        -e BIND9_USER=bind \
         -e $timezone \
-        -v /var/lib/docker/volumes/pihole_etc:/data \
+        -v pihole_etc:/data \
         -v "$(pwd)":/backup \
         $image \
         sh -c "tar czvf /backup/pihole_etc_backup.tar.gz -C /data ."    
