d1b1848d3e1d38cf4ba882a62323787d6903a7f0
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-02-11 06:36:30 -0500

    Adds reserved IP addresses to macvlan network
    Configures auxiliary addresses to reserve specific IPs for future use
    in the macvlan network deployment.

--- src/docker/networks/deploy-macvlan.sh
+++ src/docker/networks/deploy-macvlan.sh
@@ -27,6 +27,8 @@ function deploy_Macvlan() {
         --subnet=$subnet \
         --gateway=$gatewayIP \
         --ip-range=$ipRange \
+        --aux-address="reserved1=10.0.255.144" \
+        --aux-address="reserved2=10.0.255.1" \
         -o parent="$infName" $netName
 }
 
