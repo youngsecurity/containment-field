04831920bb437cf7c8d5da4ab0f197b5464213d0
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-03-05 05:30:13 -0500

    Enhance deploy-macvlan.sh script by adding bridge deployment function and refactoring aux-address parameters for improved readability and maintainability

--- src/docker/networks/deploy-macvlan.sh
+++ src/docker/networks/deploy-macvlan.sh
@@ -4,8 +4,6 @@
 # Script Name: deploy-macvlan.sh
 # Description: This script will deploy a macvlan.
 # Author: Joseph Young <joe@youngsecurity.net>
-# Created: 2024/02/01
-# Version: 1.0
 ################################################################################
 
 # Exit immediately if a command exits with a non-zero status
@@ -16,8 +14,11 @@ infName="eth0"
 subnet="10.0.255.0/24"
 ipRange="10.0.255.144/28"
 gatewayIP="10.0.255.1"
+localDNS="10.0.255.24/32"
 netName="macvlan255"
 netType="macvlan" # choose from bridge, host, overlay, macvlan, none, custom
+auxAddress1="network-address=10.0.255.144"
+auxAddress2="broadcast-address=10.0.255.159"
 
 # Define functions
 function deploy_Macvlan() {
@@ -27,9 +28,17 @@ function deploy_Macvlan() {
         --subnet=$subnet \
         --gateway=$gatewayIP \
         --ip-range=$ipRange \
-        --aux-address="network-address=10.0.255.144" \
-        --aux-address="broadcast-address=10.0.255.159" \
+        --aux-address=$auxAddress1 \
+        --aux-address=$auxAddress2 \
         -o parent="$infName" $netName
+}
+
+function deploy_bridge() {
+    sudo ip link add "$netName" link "$infName" type "$netType" mode bridge
+    sudo ip addr add 10.0.255.60/32 dev "$netName"
+    sudo ip link set "$netName" up
+    sudo ip route add "$ipRange" dev "$netName" # Add route to the macvlan network for docker dhcp
+    sudo ip route add "$localDNS" dev "$netName" # Add route to the macvlan network for local dns
 }
 
 # Main code
