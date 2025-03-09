6f6764ec0a1b1b8b8decc97cc4c9de76567351d3
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-03-09 10:12:09 -0400

    Add new DNS A records for 'whoami' and 'nginx'; update Traefik configuration for enhanced routing

--- src/docker/containers/bind9/dns.tf
+++ src/docker/containers/bind9/dns.tf
@@ -6,3 +6,22 @@ resource "dns_a_record_set" "tfk-01" {
     ]
     ttl = 30
 }
+
+resource "dns_a_record_set" "whoami" {
+    zone = "home.youngsecurity.net."
+    name = "whoami"
+    addresses = [
+        "10.0.255.144",
+        "10.0.255.145"
+    ]
+    ttl = 30
+}
+
+resource "dns_a_record_set" "nginx" {
+    zone = "home.youngsecurity.net."
+    name = "nginx"
+    addresses = [
+        "10.0.255.146"
+    ]
+    ttl = 30
+}
