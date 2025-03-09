438cb4ba205aa1492dc66eed7a20df5ec4642178
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-03-09 01:01:30 -0500

    Update .gitignore to include .bak files; remove commented addresses from dns.tf; delete unused entrypoint.sh; add ssl-certs volume to Traefik docker-compose

--- src/docker/containers/bind9/dns.tf
+++ src/docker/containers/bind9/dns.tf
@@ -4,11 +4,5 @@ resource "dns_a_record_set" "tfk-01" {
     addresses = [
         "10.0.255.8"
     ]
-    # Use multiple addresses for round-robin DNS
-    # See https://www.terraform.io/docs/providers/dns/r/a_record_set.html
-    #addresses = [
-    #    "10.0.255.8",
-    #    "10.0.255.9"
-    #]    
     ttl = 300
 }
--- src/docker/containers/bind9/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Adjust ownership
-chown -R bind:bind /var/cache/bind
-
-# Start BIND9
-#exec /usr/sbin/named -f -u bind
