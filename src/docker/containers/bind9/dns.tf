ab9e8ef3b0e75ecd1db1e1de6d6eea658b421035
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2025-03-10 03:33:43 -0400

    Refactor DNS records from A to CNAME for 'whoami' and 'nginx'; update Traefik and Nginx configurations for improved routing and clarity

--- src/docker/containers/bind9/dns.tf
+++ src/docker/containers/bind9/dns.tf
@@ -7,21 +7,16 @@ resource "dns_a_record_set" "tfk-01" {
     ttl = 30
 }
 
-resource "dns_a_record_set" "whoami" {
+resource "dns_cname_record" "whoami" {
     zone = "home.youngsecurity.net."
     name = "whoami"
-    addresses = [
-        "10.0.255.144",
-        "10.0.255.145"
-    ]
+    cname = "tfk-01.home.youngsecurity.net."
     ttl = 30
 }
 
-resource "dns_a_record_set" "nginx" {
+resource "dns_cname_record" "nginx" {
     zone = "home.youngsecurity.net."
     name = "nginx"
-    addresses = [
-        "10.0.255.146"
-    ]
+    cname = "tfk-01.home.youngsecurity.net."
     ttl = 30
 }
